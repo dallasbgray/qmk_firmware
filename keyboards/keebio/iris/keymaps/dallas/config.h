e03796a10e9a93f2defd4eceb80c94b5dd1e852c
Dallas Gray <36283741+dallasbgray@users.noreply.github.com>
2024-10-19 16:34:57 -0700

    Develop (#2)
    * updated keymaps, new tap dance routines
    
    * add debugging, fix led issues, fix QWERTY spelling
    
    * fix tap dance bug, clarify comments

--- keyboards/keebio/iris/keymaps/dallas/config.h
+++ keyboards/keebio/iris/keymaps/dallas/config.h
@@ -2,16 +2,5 @@
 
 #pragma once
 
-
-#define SPLIT_LAYER_STATE_ENABLE
-
-// rbg
-// #define RGB_MATRIX_SLEEP // turn off effects when suspended
-// #define ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS // jellybean pattern
-// #define RGB_MATRIX_SPLIT { 28, 28 } 	// (Optional) For split keyboards, the number of LEDs connected on each half. X = left, Y = Right.
-//                               		// If reactive effects are enabled, you also will want to enable SPLIT_TRANSPORT_MIRROR
-// #define SPLIT_TRANSPORT_MIRROR
-
-
-// turn off encoder settings
-// turn on layer state syncing
+// debugging
+// #define DEBUG_MATRIX_SCAN_RATE
--- keyboards/keebio/iris/keymaps/dallas/rules.mk
+++ keyboards/keebio/iris/keymaps/dallas/rules.mk
@@ -1,7 +1,6 @@
-# VIA_ENABLE = yes
+VIA_ENABLE = yes
 TAP_DANCE_ENABLE = yes
+NKRO_ENABLE = yes
 
-# BACKLIGHT_ENABLE = yes
-# RGBLIGHT_ENABLE = yes
-# RGB_MATRIX_CUSTOM_USER = yes
-# RGBLIGHT_ENABLE = yes #MAYBE NEEDidk, underglow are addressable RGB, perkey are rgb matrix
+# debugging
+# CONSOLE_ENABLE = yes
